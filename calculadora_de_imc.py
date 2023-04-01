4380e04d8e9f52aa903f03560522aa5fbcc92fe1
MiquiasAlves <miiquiasallves2015@gmail.com>
2023-03-31 23:08:32 -0300

    Create calculadora_de_imc.py
    Esta é uma calculadora de índice de massa corporal, para ver seu peso ideal.

---
+++ calculadora_de_imc.py
@@ -0,0 +1,18 @@
+print('----'*2)('Decubra seu peso ideal') ('----*2')
+nome = str(input('Digite seu nome: ')) #nome do usúario
+peso = float(input(f'{nome}, agora me informe qual seu peso? (Kg) ')) #peso do usúario
+altura = float(input('Altura em (m)? ')) #altura do usúario em M
+imc = peso / (altura ** 2) #cálculo para chegar no indice de massa corporal (IMC)
+print(f'O imc dessa pessoa é de {imc}.')
+if imc < 16:
+    print('Subpeso Severo, procure um de nossos especialista')
+elif imc <= 19.9 and imc >= 16:
+    print('Subpeso, procure um de nossos especialista')
+elif imc <= 24.9 and imc >= 20:
+    print('Normal, parábens')
+elif imc <= 29.9 and imc >= 25:
+    print('Sobrepeso, procure um de nossos especialista')
+elif imc <= 39.9 and imc >= 30:
+    print('Obeso, procure um de nossos especialista')
+else:
+    print('Obeso mórbito, procure um de nossos especialista')
