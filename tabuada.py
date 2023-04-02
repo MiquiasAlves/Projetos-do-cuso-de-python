5a61f62f3447e60a9fee7c3b8c21449e6628725b
MiquiasAlves <miiquiasallves2015@gmail.com>
2023-04-02 18:40:53 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -2,4 +2,4 @@
 num = int(input('Digite um número inteiro: '))
 for i in range(1, 11): # Loop que dos números de 1 a 10
     resultado = num * i # Calcula o resultado da multiplicação
-    print(f"{num} x {i} = {resultado}") # mostra a linha da tabuada com o já com formato 
+    print(f'{num} x {i} = {resultado}') # mostra a linha da tabuada com o já com formato 
