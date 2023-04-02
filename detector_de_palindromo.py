2cc8d4f47d7a3eb829ebcae64474ef89dd47d29f
MiquiasAlves <miiquiasallves2015@gmail.com>
2023-04-02 19:05:31 -0300

    Create detector_de_palindromo.py

---
+++ detector_de_palindromo.py
@@ -0,0 +1,16 @@
+frase = input("Digite uma frase para descobrir se é um palíndromo: ")
+frase_sem_espacos = frase.replace(" ", "").lower() # Remove os espaços da frase e converte tudo para minúsculas
+frase_invertida = frase_sem_espacos[::-1] # Inverte a frase sem espaços
+# Verifica se a frase original sem espaços é igual à frase invertida
+if frase_sem_espacos == frase_invertida:
+    print("A frase é um palíndromo.")
+else:
+    print("A frase não é um palíndromo.")
+
+# Exemplos:
+# Apos a sopa
+# A sacada da casa
+# A torre da derrota 
+# O lobo ama o bolo 
+# A base do teto desaba
+# A mala nada na lama
