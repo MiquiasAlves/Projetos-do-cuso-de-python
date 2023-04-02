55451b1a02e8cca14aaa80772bc47cbc5faf281f
MiquiasAlves <miiquiasallves2015@gmail.com>
2023-04-02 18:35:58 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,13 +1,5 @@
-n1 = int(input('Digite um número para ver a tabuada: '))
-print('-' * 44)
-print('{} x {:2} = {}'.format(n1, 1, n1*1))
-print('{} x {:2} = {}'.format(n1, 2, n1*2))
-print('{} x {:2} = {}'.format(n1, 3, n1*3))
-print('{} x {:2} = {}'.format(n1, 4, n1*4))
-print('{} x {:2} = {}'.format(n1, 5, n1*5))
-print('{} x {:2} = {}'.format(n1, 6, n1*6))
-print('{} x {:2} = {}'.format(n1, 7, n1*7))
-print('{} x {:2} = {}'.format(n1, 8, n1*8))
-print('{} x {:2} = {}'.format(n1, 9, n1*9))
-print('{} x {} = {}'.format(n1, 10, n1*10))
-print('-' * 44)
+# Solicita que o usuário digite um número inteiro
+num = int(input('Digite um número inteiro: '))
+for i in range(1, 11): # Loop que dos números de 1 a 10
+    resultado = num * i # Calcula o resultado da multiplicação
+    print(f"{num} x {i} = {resultado}") # mostra a linha da tabuada com o já com formato 
