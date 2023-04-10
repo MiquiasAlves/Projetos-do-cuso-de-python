1271c28b030e5c12e733b20cf200cd110231fb97
MiquiasAlves <miiquiasallves2015@gmail.com>
2023-04-09 23:24:05 -0300

    Create treinando_estrutura_de_dados.py

---
+++ treinando_estrutura_de_dados.py
@@ -0,0 +1,15 @@
+# Criando uma lista de frutas
+frutas = ["maçã", "banana", "laranja", "abacaxi"]
+# Imprimindo a lista de frutas
+print("Lista de frutas:")
+print(frutas)
+# Adicionando uma fruta à lista
+frutas.append("uva")
+# Imprimindo a lista de frutas atualizada
+print("Lista de frutas atualizada:")
+print(frutas)
+# Removendo uma fruta da lista
+frutas.remove("banana")
+# Imprimindo a lista de frutas atualizada novamente
+print("Lista de frutas atualizada novamente:")
+print(frutas)
